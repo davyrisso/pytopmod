44a4fd89147e82a5cb0c035bc840ebc6a027e95c
davyrisso <davyrisso@gmail.com>
2023-12-10 16:48:56 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="pytopmod",
     author="Davy Risso <davy.risso@gmail.com>",
     version="0.1.0",
+    packages=["pytopmod"],
+    package_dir={"pytopmod": "src/pytopmod"},
     package_data={"pytopmod": ["py.typed"]},
-    packages=["pytopmod"]
 )
