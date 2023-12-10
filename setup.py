6fcebf802628aa715b1b926f29f351abab785c95
davyrisso <davyrisso@gmail.com>
2023-12-10 16:45:45 +0100

    Switch from pyproject.toml to setup.py to enable auto type checking

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pytopmod",
+    author="Davy Risso <davy.risso@gmail.com>",
+    version="0.1.0",
+    package_data={"pytopmod": ["py.typed"]},
+    packages=["pytopmod"]
+)
